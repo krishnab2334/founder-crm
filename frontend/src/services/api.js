787314c6b3ee730d338afdaf50c22239c4258a89
krishnab2334 <bhandaimanisha1981@gmail.com>
2025-10-03 22:43:53 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = 'https://founder-crm-1.onrender.com/api';
 
 // Create axios instance with default config
 const api = axios.create({
